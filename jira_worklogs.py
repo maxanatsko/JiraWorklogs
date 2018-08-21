c4d4b8d089cfdb9511170ffb04ab1de6af119792
Max <maxanatsko@gmail.com>
2018-08-21 16:29:48 +0200

    Start

---
+++ jira_worklogs.py
@@ -0,0 +1,54 @@
+from jira import JIRA
+import file_writer
+from config_reader import config as cr
+from telegram_notifications import send_telegram_message as stm
+from logger import logger
+
+stm("Jira worklogs script started ▶")
+try:
+    # logging
+    logger.info('Process Started')
+    print('Started logging')
+
+    # Get data from config file
+    username = cr.get('Auth', 'username')
+    password = cr.get('Auth', 'password')
+    jira_url = cr.get('Server', 'jira_url')
+    jql_query = cr.get('Jira', 'jql_query')
+    max_results = cr.get('Jira', 'max_results')
+    file_name = cr.get('Results', 'file_name')
+    print('Finished reading config file')
+
+    options = {
+        'server': jira_url,
+        'async': True}
+
+    print('Establishing connection to Jira')
+    jira = JIRA(options, basic_auth=(username, password))
+
+    # Get all projects viewable by anonymous users.
+    # projects = jira.projects()
+    # print(projects)
+    print('Connected to Jira')
+
+    print('Retrieving issues from Jira')
+    issue_keys_list = jira.search_issues(jql_query, maxResults=300, startAt=0) + jira.search_issues(jql_query, maxResults=max_results, startAt=300)
+
+    worklog_ids = []
+    worklogs = []
+
+    print('Retrieving worklogs from the issues')
+    for ikl in issue_keys_list:
+        worklog_ids = jira.worklogs(ikl)
+        for wk in worklog_ids:
+            worklogs.append(wk.raw)
+
+    print('Finished getting worklogs')
+    print('Writing data to disk')
+    file_writer.write_data_to_file(file_name, worklogs)
+
+    # finish timestamp in the log
+    logger.info('Process Finished')
+    stm("Jira worklogs script finished successfully ✅")
+except:
+    stm("Jira worklogs script terminated with error ⛔")
